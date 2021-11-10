c3a4fda2a13129cba71d7ace4822515bb1fcb81a
Hufan <hufany21@gmail.com>
2021-11-10 16:10:29 -0500

    add redirect to longUrl and add new shorturl-longurl key value pair to database

--- express_server.js
+++ express_server.js
@@ -37,13 +37,19 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;  
+  res.redirect(`/urls/${shortURL}`)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
